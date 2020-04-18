926df4c8aa4bea639ac58770a07902b047ba0785
Alex Todoran <todoran_alex@yahoo.com>
2020-04-18 17:43:47 +0300

    Let em breathe

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Icon extends Component {
   render() {
     return (
-      <i className={this.props.icon} style={{fontSize: this.props.size + 'px', color: this.props.color}}></i>
+      <i className={this.props.icon} style={{fontSize: this.props.size + 'px', color: this.props.color, padding: "5px"}}></i>
     )
   }
 }
