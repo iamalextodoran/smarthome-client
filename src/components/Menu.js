05bf28c717300634d428ccc3f861a406d1a53299
Alex Todoran <todoran_alex@yahoo.com>
2020-04-22 14:41:34 +0300

    Current route gives primary to the menu item

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import Button from './Button';
 import '../styles/default.scss'
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default class Menu extends Component {
   render() {
     return (
-      <div className="layout-row layout-align-end-end" style={{marginRight: 10 + 'px'}}>
-        
-        <Link to="/"><Button text="Home" /></Link>
-        <Link to="/rooms"><Button text="Rooms" /></Link>
-        <Link to="/devices"><Button text="Devices" /></Link>
-        
+      <div className="layout-row layout-align-end-end" style={{marginRight: "10px"}}>
+
+        <NavLink exact activeClassName="m_button_not_raised primary" className="m_button_not_raised" to="/">Home</NavLink>
+        <NavLink activeClassName="m_button_not_raised primary" className="m_button_not_raised" to="/rooms">Rooms</NavLink>
+        <NavLink activeClassName="m_button_not_raised primary" className="m_button_not_raised" to="/devices">Devices</NavLink>
+
       </div>
     )
   }
