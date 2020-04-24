4ac2889dd65e32c06128ce5b566794dbd990c2d1
Alex Todoran <todoran_alex@yahoo.com>
2020-04-24 05:40:18 +0300

    Working ish

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react'
 import '../styles/radio.scss'
 
 export default class Radio extends Component {
+  state = {
+    checked: this.props.checked,
+  }
+
+  handleChange = (event) => {
+    this.setState({ checked: !this.state.checked  });
+    if (typeof this.props.onChange === "function") this.props.onChange();
+    this.props.setDayMode(this.state.checked);
+    this.props.getLightsToggle(this.state.checked);
+  }
+
   render() {
     return (
-      <div className="wrapper">
-        <input className="state" type="radio" name={this.props.name} id={this.props.name} value={this.props.name}/>
+      <div className="wrapper" checked={this.props.checked} onClick={this.handleChange}>
+        <input
+          className="state"
+          type="radio"
+          id={this.props.id}
+          name={this.props.name}
+          value={this.props.value}
+          checked={this.state.checked}
+        />
         <label className="label">
           <div className="indicator"></div>
           <span className="text">{this.props.text}</span>
--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -8,7 +8,32 @@ export default class Temperature extends Component {
   state = {
     setTemperature: 22,
     actualTemperature: 24,
-    mode: "eco", ///"day" or "night"
+    selectedMode: 'Eco',
+    setTemperatureEco: 16,
+    setTemperatureDay: 19,
+    setTemperatureNight: 22,
+  }
+
+  handleOptionChange = (changeEvent) => {
+    this.setState({
+      selectedMode: changeEvent.target.value
+    });
+    console.log(changeEvent)
+  }
+
+  setDayMode = (item) => {
+    console.log(item);
+    this.setState({selectedMode: item})
+  }
+  
+  setEcoMode = (item) => {
+    console.log(item);
+    this.setState({selectedMode: item})
+  }
+
+  setNightMode = (item) => {
+    console.log(item);
+    this.setState({selectedMode: item})
   }
 
   render() {
@@ -22,9 +47,9 @@ export default class Temperature extends Component {
           </div>
         </div>
         <div className="layout-row layout-align-space-between-center">
-          <Radio name="day" text="DAY" />
-          <Radio name="eco" text="ECO" />
-          <Radio name="night" text="NIGHT" />
+          <Radio checked={this.state.selectedMode === 'Day'} id="1" name="Day" text="Day" value="Day" onChange={this.setDayMode}/>
+          <Radio checked={this.state.selectedMode === 'Night'} id="2" name="Night" text="Night" value="Night" />
+          <Radio checked={this.state.selectedMode === 'Eco'} id="3" name="Eco" text="Eco" value="Eco"/>
         </div>
       </div>
     )
