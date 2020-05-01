6cb9491723470d58c906fbc225183e17c644fcfb
Alex Todoran <todoran_alex@yahoo.com>
2020-05-01 17:00:06 +0300

    Range slider with fill

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -13,10 +13,14 @@ export default class Slider extends Component {
   render() {
     return (
       <div className="layout-column layout-align-center-center">
-        <p style={{margin: "-15px"}}>{this.props.description}</p>
-        <div className="slider-container layout-row layout-align-center-center">
-          <input className="slider" type="range" min={this.props.min} max={this.props.max} onChange={this.handleChange} value={this.state.value} />
-          <p style={{paddingTop: "10px"}}>{this.state.value}%</p>
+        <p style={{ margin: "-15px" }}>{this.props.description}</p>
+        <div className="slider-container layout-row layout-align-space-between-center">
+          <div style={{width: "100%"}}>
+            <div className="slider-bar shadow" style={{ }}></div>
+            <div className="slider-bar" style={{ width: this.state.value + "%", background: this.props.color}}></div>
+            <input className="slider" type="range" min={this.props.min} max={this.props.max} onChange={this.handleChange} value={this.state.value} style={{position: "relative"}}/>
+          </div>
+          <p className="slider value">{this.state.value}%</p>
         </div>
       </div>
     )
