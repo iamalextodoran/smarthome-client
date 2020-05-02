126c7e439ee63600f702fd8802f62b7ec11cbac8
Alex Todoran <todoran_alex@yahoo.com>
2020-05-02 16:32:58 +0300

    Updates

---
+++ precache-manifest.421a5bcad4e60d6faf8a07794e20e104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2726f5e169529b77637594d0e92e227",
+    "url": "/react-neumorphic/index.html"
+  },
+  {
+    "revision": "6589406cddbde4d32489",
+    "url": "/react-neumorphic/static/css/main.47bcb239.chunk.css"
+  },
+  {
+    "revision": "b2133bf7cd662a9f392e",
+    "url": "/react-neumorphic/static/js/2.0b000c23.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/react-neumorphic/static/js/2.0b000c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6589406cddbde4d32489",
+    "url": "/react-neumorphic/static/js/main.a9450f73.chunk.js"
+  },
+  {
+    "revision": "463cfff93f84c1b443df",
+    "url": "/react-neumorphic/static/js/runtime-main.4a652ea4.js"
+  }
+]);
