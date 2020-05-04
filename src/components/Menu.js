7dfc95e9f026f600a577a2d5b13d623e901b7481
Alex Todoran <todoran_alex@yahoo.com>
2020-05-05 01:42:01 +0300

    Darkmode and scrollbar styling

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Button from './Button';
-import '../styles/default.scss'
+import '../styles/menu.scss'
 import { NavLink } from "react-router-dom";
 import Icon from './Icon';
 
@@ -8,16 +8,16 @@ export default class Menu extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="big layout-row layout-align-end-end" style={{ marginRight: "10px" }}>
+        <div className="menu big layout-row layout-align-end-end">
           <NavLink exact activeClassName="m_button_not_raised primary" className="m_button_not_raised" to="/">Home</NavLink>
           <NavLink activeClassName="m_button_not_raised primary" className="m_button_not_raised" to="/rooms">Rooms</NavLink>
           <NavLink activeClassName="m_button_not_raised primary" className="m_button_not_raised" to="/devices">Devices</NavLink>
         </div>
 
-        <div className="small" style={{ marginRight: "10px" }}>
-          <NavLink exact style={{color: "black"}} activeStyle={{color: "coral"}} to="/"><Icon icon="fas fa-home" size="25"/></NavLink>
-          <NavLink style={{color: "black"}} activeStyle={{color: "coral"}} to="/rooms"><Icon icon="fas fa-square" size="25" /></NavLink>
-          <NavLink style={{color: "black"}} activeStyle={{color: "coral"}} to="/devices"><Icon icon="fas fa-toolbox" size="25" /></NavLink>
+        <div className="menu small">
+          <NavLink exact className="menu-item" activeClassName="menu-item-active" to="/"><Icon icon="fas fa-home" size="25"/></NavLink>
+          <NavLink className="menu-item" activeClassName="menu-item-active" to="/devices"><Icon icon="fas fa-toolbox" size="25" /></NavLink>
+          <NavLink className="menu-item" activeClassName="menu-item-active" to="/rooms"><Icon icon="fas fa-square" size="25" /></NavLink>
         </div>
       </React.Fragment>
     )
