ded04c79f938cdc4e89508504e5d35bb452a7cee
Alex Todoran <todoran_alex@yahoo.com>
2020-05-06 13:56:04 +0300

    User should be able to click on either the label or the button to change it

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -2,31 +2,21 @@ import React, { Component } from 'react'
 import '../styles/radio.scss'
 
 export default class Radio extends Component {
-  state = {
-    checked: this.props.checked,
-  }
-
-  handleChange = (event) => {
-    this.setState({ checked: !this.state.checked });
-    if (typeof this.props.onChange === "function") this.props.onChange();
-    this.props.setMode(event.target.checked, event.target.innerHTML);
-  }
-
   render() {
     return (
-      <div className="wrapper" value={this.props.value} checked={this.props.checked} onClick={this.handleChange}>
+      <div className="wrapper" checked={this.props.checked} onClick={this.props.onClick}>
         <input
           className="state"
           type="radio"
           id={this.props.value}
           name={this.props.value}
           value={this.props.value}
-          checked={this.state.checked}
-          onChange={this.handleChange}
+          checked={this.props.checked}
+          onChange={this.props.onChange}
         />
         <label className="label">
-          <div className="indicator" value={this.props.value}></div>
-          <span className="text" value={this.props.value}>{this.props.value}</span>
+          <div className="indicator"></div> 
+          <span className="text">{this.props.value}</span>
         </label>
       </div>
     )
--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -14,20 +14,27 @@ export default class Temperature extends Component {
     setTemperatureNight: 22,
   }
 
-  setMode = (state, item) => {
-    let setThis;
-    !state ? this.setState({ selectedMode: item }) : this.setState({ selectedMode: this.state.selectedMode });
-    if (item === 'Eco') {
+  handleClick = () => {
+    console.log(this.props.value)
+  }
+
+  changeTemperatureMode = (e) => {
+    const value = e.target.innerHTML === '' ? this.state.selectedMode: e.target.innerHTML;
+    let setThis = '';
+    
+    this.setState({ selectedMode: value });
+
+    if (value === 'Eco') {
       setThis = this.state.setTemperatureEco;
-    } else if (item === 'Day') {
+    } else if (value === 'Day') {
       setThis = this.state.setTemperatureDay;
-    } else if (item === 'Night') {
+    } else if (value === 'Night') {
       setThis = this.state.setTemperatureNight;
     }
     this.setState({ setTemperature: setThis });
   }
 
-  setColor = () => {
+  setThermometerColor = () => {
     if (this.state.selectedMode === 'Day') {
       return "deeppink"
     } else if (this.state.selectedMode === 'Night') {
@@ -39,16 +46,16 @@ export default class Temperature extends Component {
     return (
       <div className="card">
         <div className="layout-row layout-align-center-center">
-          <Icon icon={this.state.selectedMode === 'Eco' ? 'fas fa-thermometer-empty' : 'fas fa-thermometer-half'} size="40" color={this.setColor()} />
+          <Icon icon={this.state.selectedMode === 'Eco' ? 'fas fa-thermometer-empty' : 'fas fa-thermometer-half'} size="40" color={this.setThermometerColor()} />
           <div className="layout-column layout-align-end-end">
             <h2><strong>{this.state.actualTemperature}  °C</strong></h2>
             <p style={{ marginTop: "-25px" }}>set {this.state.setTemperature} °C</p>
           </div>
         </div>
         <div className="layout-row layout-align-space-between-center" style={{ marginTop: "-20px" }}>
-          <Radio checked={this.state.selectedMode === 'Day'} value="Day" setMode={this.setMode} />
-          <Radio checked={this.state.selectedMode === 'Eco'} value="Eco" setMode={this.setMode} />
-          <Radio checked={this.state.selectedMode === 'Night'} value="Night" setMode={this.setMode} />
+          <Radio checked={this.state.selectedMode === 'Day'} value="Day" onClick={this.changeTemperatureMode} />
+          <Radio checked={this.state.selectedMode === 'Eco'} value="Eco" onClick={this.changeTemperatureMode} />
+          <Radio checked={this.state.selectedMode === 'Night'} value="Night" onClick={this.changeTemperatureMode} />
         </div>
       </div>
     )
