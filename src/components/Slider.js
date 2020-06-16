43557973161aaf819efca73af6370c06b6976f5b
Todoran Alex <todoran_alex@yahoo.com>
2020-06-16 07:17:52 +0300

    Corrected errors for showing 30 when actually 0 value is given

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react'
 import '../styles/slider.scss'
 
 export default class Slider extends Component {
-  state = {
-    value: this.props.value || 30,
-  }
-
-  handleChange = (e) => {
-    this.setState({ value: e.target.value })
-  }
-
   render() {
     return (
       <div className="layout-column layout-align-center-center">
@@ -17,10 +9,10 @@ export default class Slider extends Component {
         <div className="slider-container layout-row layout-align-space-between-center">
           <div style={{ width: "100%" }}>
             <div className="slider-bar shadow" style={{}}></div>
-            <div className="slider-bar" style={{ width: this.state.value + "%", background: this.props.color }}></div>
-            <input className="slider" type="range" step={this.props.step} min={this.props.min} max={this.props.max} onChange={this.handleChange} value={this.state.value} style={{ position: "relative" }} />
+            <div className="slider-bar" style={{ width: this.props.value + "%", background: this.props.color }}></div>
+            <input className="slider" type="range" step={this.props.step} min={this.props.min} max={this.props.max} onChange={this.props.onChange} value={this.props.value} style={{ position: "relative" }} />
           </div>
-          <p className="slider-value">{this.state.value}%</p>
+          <p className="slider-value">{this.props.value}%</p>
         </div>
       </div>
     )
